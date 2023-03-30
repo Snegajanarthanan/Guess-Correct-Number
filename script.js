b57e52dc0d1f9d5ddee1c5511c363d2e7028d9c0
Snegajanarthanan <snega.janarthanan@gmail.com>
2023-03-30 21:09:38 +0100

    final changes

--- script.js
+++ script.js
@@ -3,9 +3,9 @@
 console.log(document.querySelector('.message').textContent);
 document.querySelector('.message').textContent = '🎉correct Number!';
 
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
-document.querySelector('.guess').value = 23;
+document.querySelector('.number').textContent;
+document.querySelector('.score').textContent = 20;
+document.querySelector('.guess').value;
 console.log(document.querySelector('.guess').value);
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
